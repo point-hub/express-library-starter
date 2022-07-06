89dcc64a1ca18b6167c682efae06a0a6fac0648f
Martien Dermawan Tanama <martiendt@gmail.com>
2022-07-06 15:17:28 +0700

    replace ts-jest with babel-jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,18 +1,13 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 
 module.exports = {
-  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
   roots: ["src"],
   bail: 1,
   collectCoverageFrom: ["src/**/*.ts"],
+  extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
     "^@src/(.*)\\.js$": "<rootDir>/src/$1",
     "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
   },
 };
